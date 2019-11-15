5be78e59a57aabdbc54790415be347bd81c198e0
smeggins <phillipchadwick@hotmail.com>
2019-11-14 20:49:20 -0800

    adds eslint to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,40 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser'),
       rename = require('gulp-rename');
+      browserSync = require('browser-sync').create();
+      eslint = require('gulp-eslint');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('scripts', function() {
     return gulp.src('./src/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
     .pipe(terser())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('./build/js'))
 })
 
-gulp.task('default', function(done) {
-    console.log('hello world')
+gulp.task('reload', function(done) {
+    browserSync.reload();
     done();
 })
 
-gulp.task('default', gulp.parallel('scripts'))
+gulp.task('watch', function() {
+    gulp.watch('./src/*.js', gulp.parallel('scripts', 'reload'));
+    gulp.watch('index.html', gulp.parallel('reload'))
+});
+
+
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'))
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const a = 1;
 const b = 2;
 let c = a+b;
+
+let d =c-a+b
+
+let test = document.getElementById('test'  )
+test.innerhtml = 'yo momma'
+document.appendChild(test)
+
+let z = 2
+
+let r = 'yo mama'
